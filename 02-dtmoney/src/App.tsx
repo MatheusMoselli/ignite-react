c52400aa738fafa25d806b32880179dba2956162
Matheus Moselli <62854829+MatheusMoselli@users.noreply.github.com>
2021-12-23 11:15:19 -0300

    Primeira aplicação web com ReactJS - Sections 1,2, 3

---
+++ 02-dtmoney/src/App.tsx
@@ -0,0 +1,35 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
+
+export const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+    </>
+  );
+};
