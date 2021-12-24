ade0efab6941c1f185a07a5aacdc5ebc65ef95d0
Matheus Moselli <62854829+MatheusMoselli@users.noreply.github.com>
2021-12-24 10:23:25 -0300

    NextJS Fundamentals - Section 1

---
+++ 03-ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Hello world!</h1>
+    </>
+  );
+};
